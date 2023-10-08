ca9ca0a1aaa3d178ae799628b9d44c6be58f69f0
eqawasm <eqawasm@gmail.com>
2023-10-08 15:09:55 -0400

    Add aboutus info page

--- lab5/shoppers_shenanigans/src/components/about.js
+++ lab5/shoppers_shenanigans/src/components/about.js
@@ -1,14 +1,45 @@
 import React from "react";
+import "./about.css";
+import OSUlogo from "./OSUlogo.jpg";
 
 const About = () => {
-  
+  const teamMembers = [
+    {
+      name: "Ethar Qawasmeh",
+      jobTitle: "CEO",
+      image: "OSUlogo.jpg",
+      bio: "Ethar is the Chief Executive Officer at our company. With over 10 years of experience in software development, he leads our team in creating innovative solutions for our clients."
+    },
+    {
+      name: "Anna Dubey",
+      jobTitle: "CFO",
+      image: "OSUlogo.jpg",
+      bio: "Anna is the Chief Financial Officer at our company. She is responsible for managing our financial operations and ensuring the company's financial stability and growth."
+    },
+    {
+      name: "Prithviraj Patel",
+      jobTitle: "CTO",
+      image: "OSUlogo.jpg",
+      bio: "Prith is the Chief Technology Officer at our company. He oversees our technical team and is responsible for driving our technology strategy and innovation."
+    }
+  ];
+
   return (
     <div>
-      <h1>
-        about page
-      </h1>
+      <h1>About Us</h1>
+      <h2>Our Team</h2>
+      <div className="team-members">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="team-member">
+            <img src={OSUlogo} alt={member.name} />
+            <h3>{member.name}</h3>
+            <p><strong>Job Title:</strong> {member.jobTitle}</p>
+            <p>{member.bio}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
-  
+
 export default About;
