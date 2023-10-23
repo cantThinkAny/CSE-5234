c2646a82934cb93b37aff2eafe29d89ca6c1031b
Prithviraj Patel <prithvirajpatel@Prithvirajs-MBP.lan>
2023-10-22 20:16:25 -0400

    potential bug found

--- lab5/shoppers_shenanigans/server/server.js
+++ lab5/shoppers_shenanigans/server/server.js
@@ -23,24 +23,62 @@ app.get("/", function(req,res) {
 }) ;
 
 // DROP table if exists, then CREATE
-const qrop_item = "DROP TABLE IF EXISTS Item"; 
+const drop_item = "DROP TABLE IF EXISTS Item"; 
 const creat_item = "CREATE TABLE Item ( \
     Id int NOT NULL AUTO_INCREMENT, \
     Item varchar(255), \
     quantity int, \
     PRIMARY KEY (Id) );" ; 
 
-// INSERT default data
+const drop_inventory = "DROP TABLE IF EXISTS Invetory";
+const create_inventory = "CREATE TABLE Invetory ( \
+    Id int NOT NULL AUTO_INCREMENT, \
+    Product name varchar(255), \
+    Quantity int, \
+    PRIMARY KEY (Id) );" ;
 
+
+const drop_order_processing = "DROP TABLE IF EXISTS OrderProcessing";
+const create_order_processing = "CREATE TABLE OrderProcessing ( \
+    Order Id int NOT NULL AUTO_INCREMENT, \
+    ProductName varchar(255), \
+    ProductQuantity int, \
+    PRIMARY KEY (Order Id) );" ;
+// INSERT default data
+//________Prithviraj:  I think this maybe conflicting with the Purchase page dialog box numbers, hence the bug we found while running it today. _____Please Confirm :/
 const item_add1 = "INSERT INTO Item (Item, quantity) VALUES ('product 1', 10)" 
 const item_add2 = "INSERT INTO Item (Item, quantity) VALUES ('product 2', 20)"
 
+const inventory_add1 = "INSERT INTO Invetory (ProductName, Quantity) VALUES ('product 1', 10)"
+const inventory_add2 = "INSERT INTO Invetory (ProductName, Quantity) VALUES ('product 2', 20)"
+
+const order_processing_add1="INSERT INTO OrderProcessing (ProductName, ProductQuantity) VALUES ('product 1', 10)";
+const order_processing_add2="INSERT INTO OrderProcessing (ProductName, ProductQuantity) VALUES ('product 2', 20)";
+
 app.get("/get_item", function (req, res) {
-    db.query(qrop_item);
+    db.query(drop_item);
     db.query(creat_item);
     db.query(item_add1);
     db.query(item_add2);
     const result = db.query('select * from Item');
+    return res.send(result)
+});
+
+app.get("/get_inventory", function (req, res) {
+    db.query(drop_inventory);
+    db.query(create_inventory);
+    db.query(inventory_add1);
+    db.query(inventory_add2);
+    const result = db.query('select * from Inventory');
+    return res.send(result)
+});
+
+app.get("/get_orderprocessing", function (req, res) {
+    db.query(drop_order_processing);
+    db.query(create_order_processing);
+    db.query(inventory_add1);
+    db.query(inventory_add2);
+    const result = db.query('select * from Inventory');
     return res.send(result)
 });
 
