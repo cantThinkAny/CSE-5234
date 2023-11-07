dfa8a017f2ece4b89ad6d167070e76d75ae8c806
Prithviraj Patel <prithvirajpatel@Prithvirajs-MBP.lan>
2023-11-06 19:20:37 -0500

    modified the purchase page

--- lab5/shoppers_shenanigans/src/components/purchase.js
+++ lab5/shoppers_shenanigans/src/components/purchase.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import hoodie1 from "./images/hoodie1.jpg";
+import hoodie12 from "./images/hoodie1-2.jpg";
+import hoodie2 from "./images/hoodie2.jpg";
+import hoodie22 from "./images/hoodie2-1.jpg";
+import "./purchase.css";
 const Purchase = () => {
   const [order, setOrder] = useState({
-    buyQuantity: [0, 0, 0, 0, 0],
+    buyQuantity: [0, 0],
     credit_card_number: "",
     expir_date: "",
     cvvCode: "",
@@ -12,13 +16,14 @@ const Purchase = () => {
     address_2: "",
     city: "",
     state: "",
-    zip: ""
+    zip: "",
   });
-
+  const [firstImage, setFirstImage] = useState(true);
+  const [secondImage, setSecondImage] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    // e.preventDefault(); // Prevent form submission
+    e.preventDefault(); // Prevent form submission
     navigate("/purchase/paymentEntry", { state: { order } });
   };
 
@@ -26,33 +31,83 @@ const Purchase = () => {
     // Create a copy of the 'buyQuantity' array to update it
     const updatedQuantity = [...order.buyQuantity];
     updatedQuantity[index] = value;
-    
+
     // Update the 'order' state with the new quantity
     setOrder((prevOrder) => ({
       ...prevOrder,
-      buyQuantity: updatedQuantity
+      buyQuantity: updatedQuantity,
     }));
+  };
+
+  const onClickImageFlip = (image) => {
+    if (image === 0) {
+      setFirstImage(!firstImage);
+    } else {
+      setSecondImage(!secondImage);
+    }
   };
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
-        <label>Product 1</label>
-        <input
-          type="number"
-          required
-          value={order.buyQuantity[0]}
-          onChange={(e) => handleQuantityChange(0, e.target.value)}
-        />
-        <br />
-        <label>Product 2</label>
-        <input
-          type="number"
-          required
-          value={order.buyQuantity[1]}
-          onChange={(e) => handleQuantityChange(1, e.target.value)}
-        />
-        <br />
+      <div className="product-contianer">
+        <div className="product-1">
+          <div className="image-container">
+            <img
+              src={firstImage ? hoodie1 : hoodie12}
+              alt={firstImage ? "naruto" : "naruto-2"}
+              className="scrollable-image"
+            />
+            <br />
+            <button className="scroll-left" onClick={() => onClickImageFlip(0)}>
+              ←
+            </button>
+            <button
+              className="scroll-right"
+              onClick={() => onClickImageFlip(0)}
+            >
+              →
+            </button>
+          </div>
+          <label>Product 1</label>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="number"
+              required
+              value={order.buyQuantity[0]}
+              onChange={(e) => handleQuantityChange(0, e.target.value)}
+            />
+          </form>
+        </div>
+        <div className="product-2">
+          <div className="image-container">
+            <img
+              src={secondImage ? hoodie2 : hoodie22}
+              alt={secondImage ? "gaara" : "gaara-2"}
+              className="scrollable-image"
+            />
+            <br />
+            <button className="scroll-left" onClick={() => onClickImageFlip(1)}>
+              ←
+            </button>
+            <button
+              className="scroll-right"
+              onClick={() => onClickImageFlip(1)}
+            >
+              →
+            </button>
+          </div>
+          <label>Product 2</label>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="number"
+              required
+              value={order.buyQuantity[1]}
+              onChange={(e) => handleQuantityChange(1, e.target.value)}
+            />
+          </form>
+        </div>
+      </div>
+      <form onSubmit={handleSubmit} className="pay-button">
         <button className="button">Pay</button>
       </form>
     </div>
