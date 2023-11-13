81c4f3f8c96b7bd7eb7a6b8114b54ea4243ead82
Prithviraj Patel <prithvirajpatel@Prithvirajs-MacBook-Pro.local>
2023-11-13 18:24:24 -0500

    port number(7000->7007) and product 1&2 changed

--- lab5/shoppers_shenanigans/server/server.js
+++ lab5/shoppers_shenanigans/server/server.js
@@ -1,57 +1,62 @@
-const express = require("express"); 
-const db = require('./database'); 
-const app = express(); 
-const port = 7000; 
-const cors = require('cors'); 
+const express = require("express");
+const db = require("./database");
+const app = express();
+const port = 7000;
+const cors = require("cors");
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    credentials: true,
-    optionSuccessStatus: 200
-}
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
 
-app.use(cors(corsOptions)); 
+app.use(cors(corsOptions));
 
-//set access post 
-app.use(express.json()); 
+//set access post
+app.use(express.json());
 app.listen(port, () => {
-    console.log(`RUN http://localhost:${port}`)
+  console.log(`RUN http://localhost:${port}`);
 });
 
-app.get("/", function(req,res) {
-    res.send("Hello World!")
-}) ;
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
 
 // DROP table if exists, then CREATE
-const qrop_item = "DROP TABLE IF EXISTS Item"; 
-const creat_item = "CREATE TABLE Item ( \
+const qrop_item = "DROP TABLE IF EXISTS Item";
+const creat_item =
+  "CREATE TABLE Item ( \
     Id int NOT NULL AUTO_INCREMENT, \
     Item varchar(255), \
     quantity int, \
-    PRIMARY KEY (Id) );" ; 
+    PRIMARY KEY (Id) );";
 
 // INSERT default data
 
-const item_add1 = "INSERT INTO Item (Item, quantity) VALUES ('product 1', 10)" 
-const item_add2 = "INSERT INTO Item (Item, quantity) VALUES ('product 2', 20)"
+const item_add1 =
+  "INSERT INTO Item (Item, quantity) VALUES ('Naruto Hoodie', 10)";
+const item_add2 =
+  "INSERT INTO Item (Item, quantity) VALUES ('Gaara Hoodie', 20)";
 
 app.get("/get_item", function (req, res) {
-    db.query(qrop_item);
-    db.query(creat_item);
-    db.query(item_add1);
-    db.query(item_add2);
-    const result = db.query('select * from Item');
-    return res.send(result)
+  db.query(qrop_item);
+  db.query(creat_item);
+  db.query(item_add1);
+  db.query(item_add2);
+  const result = db.query("select * from Item");
+  return res.send(result);
 });
 
-app.post("/update_quantity", function(req, res) { 
-    var IDs = req.body.names;
-    var quantity = req.body.quantity;
+app.post("/update_quantity", function (req, res) {
+  var IDs = req.body.names;
+  var quantity = req.body.quantity;
 
-    IDs.forEach((id_, index) => {
-        const quan = quantity[index];
-        const result1 = db.query(`UPDATE Item SET quantity = ${quan} WHERE Id = ${id_};`);
-    });
+  IDs.forEach((id_, index) => {
+    const quan = quantity[index];
+    const result1 = db.query(
+      `UPDATE Item SET quantity = ${quan} WHERE Id = ${id_};`
+    );
+  });
 
-    return res.send('')
+  return res.send("");
 });
--- lab5/shoppers_shenanigans/src/components/purchase.js
+++ lab5/shoppers_shenanigans/src/components/purchase.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios"; 
+import axios from "axios";
 
 const Purchase = () => {
   const [order, setOrder] = useState({
@@ -13,37 +13,45 @@ const Purchase = () => {
     address_2: "",
     city: "",
     state: "",
-    zip: ""
+    zip: "",
   });
 
-  useEffect(()=>{
-      // query the order info
-      axios.get("http://localhost:7000/get_item", {
-        params: {} 
-      }).then((data)=>{
+  useEffect(() => {
+    // query the order info
+    axios
+      .get("http://localhost:7000/get_item", {
+        params: {},
+      })
+      .then((data) => {
         const data_ = JSON.parse(JSON.stringify(data.data));
-        data_.forEach(order_ => {
-          order.buyQuantity[order_.Id-1] = order_.quantity
+        data_.forEach((order_) => {
+          order.buyQuantity[order_.Id - 1] = order_.quantity;
         });
-        setOrder({...order});
+        setOrder({ ...order });
       });
-  },[])
+  }, []);
 
-
-  const purchaseHandler = event => { 
+  const purchaseHandler = (event) => {
     event.preventDefault();
-    const array1  = [1,2];
+    const array1 = [1, 2];
     axios
       .post("http://localhost:7000/update_quantity", {
         names: array1,
         quantity: order.buyQuantity,
-        user_uid: -1})
-      .then((data)=>{
-        const res_data = data.data; 
-        if (res_data.length > 0) { alert(res_data); }
-        else{navigate('/purchase/paymentEntry', {state: {order: order}});}
-        })
-      .catch((e) => {alert("failed", e);});}
+        user_uid: -1,
+      })
+      .then((data) => {
+        const res_data = data.data;
+        if (res_data.length > 0) {
+          alert(res_data);
+        } else {
+          navigate("/purchase/paymentEntry", { state: { order: order } });
+        }
+      })
+      .catch((e) => {
+        alert("failed", e);
+      });
+  };
   const navigate = useNavigate();
 
   const handleSubmit = (e) => {
@@ -55,38 +63,42 @@ const Purchase = () => {
     // Create a copy of the 'buyQuantity' array to update it
     const updatedQuantity = [...order.buyQuantity];
     updatedQuantity[index] = value;
-    
+
     // Update the 'order' state with the new quantity
     setOrder((prevOrder) => ({
       ...prevOrder,
-      buyQuantity: updatedQuantity
+      buyQuantity: updatedQuantity,
     }));
   };
 
   return (
-  <div>
-    <form onSubmit={purchaseHandler}>
-      <label>Product 1</label>
-      <input
-        type="number"
-        value={order.buyQuantity[0]}
-        required onChange={(e) =>
-          { order.buyQuantity[0] = e.target.value;
-            setOrder({...order});}}
-      />
-      <br/>
-      <label >Product 2</label>
-      <input
-        type="number"
-        value={order.buyQuantity[1]}
-        required onChange={(e) =>
-          { order.buyQuantity[1] = e.target.value; 
-            setOrder({...order});}}
-      />
-      <br/>
-      <button className='button'>Pay</button>
-    </form>
-  </div>
+    <div>
+      <form onSubmit={purchaseHandler}>
+        <label>Naruto Hoodie</label>
+        <input
+          type="number"
+          value={order.buyQuantity[0]}
+          required
+          onChange={(e) => {
+            order.buyQuantity[0] = e.target.value;
+            setOrder({ ...order });
+          }}
+        />
+        <br />
+        <label>Gaara Hoodie</label>
+        <input
+          type="number"
+          value={order.buyQuantity[1]}
+          required
+          onChange={(e) => {
+            order.buyQuantity[1] = e.target.value;
+            setOrder({ ...order });
+          }}
+        />
+        <br />
+        <button className="button">Pay</button>
+      </form>
+    </div>
   );
 };
 
